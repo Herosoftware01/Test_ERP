36b4d7bb4ca9f350b8d3a3540301c4dc463887a7
herosoftware01 <herosaftware@herofashion.com>
2025-05-06 17:45:36 +0530

    Emp_create

--- ERP/Production/models.py
+++ ERP/Production/models.py
@@ -1,3 +1,8 @@
+from typing import Self
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
