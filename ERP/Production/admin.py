cc9ab46f507aa6b1f12225cdecad13de9f3fbe77
herosoftware01 <herosaftware@herofashion.com>
2025-05-06 17:39:12 +0530

    Production

--- ERP/Production/admin.py
+++ ERP/Production/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import production
+
+class productionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'price')
+
+admin.site.register(production)
 
 # Register your models here.
