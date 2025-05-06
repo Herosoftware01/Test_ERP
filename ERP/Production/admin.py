35f450661d6a7e2d4fdd399116e8c7b69db0a726
Hero Software <herosoftware@herofashion.com>
2025-05-06 17:41:40 +0530

    All_Delete

--- ERP/Production/admin.py
+++ ERP/Production/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-from .models import production
-
-class productionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'quantity', 'price')
-
-admin.site.register(production)
-
-# Register your models here.
